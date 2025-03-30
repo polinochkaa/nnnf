ff1284f5fbd5886d213eba4d941b6eb231677e71
Polina Akishina <akishinapd@gmail.com>
2025-03-30 17:13:00 +0400

    Revert "Добавление файлов проекта"
    This reverts commit d58910991dfea10d8ddd5b6226e209feff6eee99.

--- helpers/hbs.js
+++
@@ -1,7 +0,0 @@
-const Handlebars = require('handlebars');
-
-Handlebars.registerHelper("equal", function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
-
-module.exports = {equal};
